0e7dde613ea37ec84bfc67de163e85bc6dadc200
itsbasilsaman <itsbasilsaman@gmail.com>
2025-06-01 09:53:45 +0530

    Changes Made

--- next.config.mjs
+++ next.config.mjs
@@ -1,1 +1,37 @@
- 
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'i.ytimg.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'tse1.mm.bing.net',
+      },
+      {
+        protocol: 'https',
+        hostname: 'tse2.mm.bing.net',
+      },
+      {
+        protocol: 'https',
+        hostname: 'tse3.mm.bing.net',
+      },
+      {
+        protocol: 'https',
+        hostname: 'plus.unsplash.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'hebbkx1anhila5yf.public.blob.vercel-storage.com',
+      }
+    ],
+  },
+};
+
+export default nextConfig; 
